f874769402570f6bb556e1e5b234048b31ad6191
Emma Paris <suginant@gmail.com>
2023-03-10 12:51:14 -0500

    Added new files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
+import { Header, Footer } from './containers';
 import { Card } from './components';
 
 const App = () => {
   return (
     <div>
         <Card />
+        
     </div>
   )
-}
+};
 
-export default App
+export default App;
