3ceb8a7efc7ad55f70b93f7603a2298c103604b0
Anton Sugin <suginant@gmail.com>
2023-06-05 21:49:24 -0400

    test

--- src/productsData.js
+++ src/productsData.js
@@ -9,7 +9,7 @@ export const data = {
             details: "lorem jnefd jnwief jnwienf",
             reviewsArray: {
                 id: "1",
-                personName: "jhduv",
+                personName: "",
                 personReview: "",
                 mer: ""
             }
